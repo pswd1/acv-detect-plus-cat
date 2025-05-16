ede56b92ebe3d194fb29ceb65205fe6716a6c3cc
rootloopz <cody@rootloopz.com>
2025-05-15 21:49:48 -0700

    okay pulled in exlcusion code and updated main

---
+++ README.md
@@ -0,0 +1,12 @@
+# Applied Computer Vision Project, motion detection, built in cat/pet filtering 
+
+![Project Screenshot](awesome-acv.png)
+
+## Requirements
+- OpenCV library must be installed (we are using 4.11.1)
+
+## Usage
+Build using the included Makefile, then run:
+```
+./main <capture device>
+```
--- main.cpp
+++ main.cpp
@@ -11,8 +11,11 @@ using namespace cv;
 using namespace std;
 
 bool motiondetect(int sensitivity, Mat prev_frame, Mat curr_frame);
+void exclusionzone(tuple<int, int> top_left,tuple<int, int> bottom_right,  Mat in_mat, Mat &ex_mat);
+
 bool catDetect(Mat frame);
 CascadeClassifier cat_cascade;
+
 
 bool motiondetect(int sensitivity, Mat prev_frame, Mat curr_frame){
   /*
@@ -30,7 +33,6 @@ TODO: a default should be determined if the sensitivity is not given or zero is 
   //for now we will just do a color diff not a grayscale
   absdiff(prev_frame, curr_frame, diff_frame);
   diff_value = (unsigned int)cv::sum(diff_frame)[0]; //add up only the blue frame
-                                                     //the line above had mat_diff i changed it to diff_frame
   if (diff_value > sensitivity){
     //motion detected
     return true;
@@ -38,6 +40,43 @@ TODO: a default should be determined if the sensitivity is not given or zero is 
   return false;
 }
 
+void exclusionzone(tuple<int, int> top_left,tuple<int, int> bottom_right,  Mat in_mat, Mat &ex_mat){
+  /*
+     example mat is 250x250
+     example inputs
+     x    y
+     topleft =   (34,  27)
+     topright =  (123, 238)
+
+*/
+  //this program will draw a white box over an area in the mat which excludes it from motion detection.
+  //get the coordinates of the tuples for top right and lower left
+  int x1 = get<0>(top_left);
+  int y1 = get<1>(top_left);
+
+  int x2 = get<0>(bottom_right);
+  int y2 = get<1>(bottom_right);
+  //clone in_mat to ex_mat
+  ex_mat = in_mat.clone();
+
+  //loop through ex_mat and overwrite pixels
+  for(int x = 0; x < ex_mat.rows; x++){
+    for(int y = 0; y < ex_mat.cols; y++){
+      //if row is in range
+      if(x >= x1 && x <=x2){
+        //if col is in range
+        if(y >= y1 && y <=y2){
+          ex_mat.at<Vec3b>(x,y)[0] = 0;
+          ex_mat.at<Vec3b>(x,y)[1] = 0;
+          ex_mat.at<Vec3b>(x,y)[2] = 0;
+          //pixel == 255
+        }
+
+      }
+
+    }
+  }
+}
 /*
 Credits:
 https://docs.opencv.org/4.11.0/db/d28/tutorial_cascade_classifier.html
@@ -73,45 +112,55 @@ bool catDetect(Mat frame)
   return true;
 }
 
-
-int main(int argc, char* argv[]){
-
+int main(int argc, char* argv[]) {
   // check for cmd line args
-  //
-  if(argc < 2){
+  if(argc < 2) {
     cerr << "Error: \n" << "Usage: ./main (capture device) \n" << "./main 1" << endl; 
     return -1;
   } else {
-    int cam = stoi(argv[1]);
-    cout << "Using Capture Device: " << cam << endl;
+    cout << "Using Capture Device: " << argv[1] << endl;
   }
 
   // open selected capture device and start capturing frames for processing
+  int cam = stoi(argv[1]);
   VideoCapture cap(cam);
-  if(!cap.isOpened())
-  {
-    cout << "--(!)Error opening camera 2\n";
+  if(!cap.isOpened()) {
+    cout << "--(!)Error opening camera\n";
     return -1;
   }
 
+  tuple<int, int> tr(170, 83);
+  tuple<int, int> bl(400, 377);
+
+  namedWindow("Original");
+  namedWindow("Excluded");
+
   Mat frame;
   Mat prev_frame;
-  while(true)
-  {
-    prev_frame = frame.clone(); // save current frame for motion detection
-    cap >> frame;
+  Mat excluded_frame;
 
-    if(frame.empty())
-    {
+  while(true) {
+    prev_frame = frame.clone(); // save current frame for motion detection
+
+    cap >> frame;
+    if(frame.empty()) {
       cout << "--(!)No frame captured from camera\n";
       break;
     }
 
-    // Check for motion, if yes pass frame to catDetect
-    //@BILLY: This segfaults when passing the values, i dont want to modify your code
-    if(motiondetect(1, prev_frame, frame) == true){
+    exclusionzone(tr, bl, frame, excluded_frame);
 
-      catDetect(frame);
+    imshow("Original", frame);
+    imshow("Excluded", excluded_frame);
+
+    if(!prev_frame.empty()) {
+      Mat excluded_prev_frame;
+      exclusionzone(tr, bl, prev_frame, excluded_prev_frame);
+
+      // Check for motion, if yes pass frame to catDetect
+      if(motiondetect(1, excluded_prev_frame, excluded_frame) == true) {
+        catDetect(excluded_frame);
+      }
     }
 
     // Exit on ESC key
